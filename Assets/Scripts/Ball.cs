d6984dc773e66a90699fb573c129c4d2c44b75a0
linneaskola <c22linkl@student.his.se>
2023-09-04 21:04:02 +0200

    bollen studsar mot spelaren

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody2D))]
 public class Ball : MonoBehaviour
 {
+    public Rigidbody2D rb;
+
+    public float speed = 1;
+    private Vector2 velocity = Vector2.down;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.velocity = velocity;
+    }   
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector2 bounce;
+        if (col.gameObject.CompareTag("Player"))
+        {
+            bounce = transform.position - col.transform.position;
+            velocity = bounce.normalized * speed;
+        }
+        else
+        {
+            //use .Reflect to studsa bollen, bollen har ingen velocity i studsen
+        }
     }
 }
