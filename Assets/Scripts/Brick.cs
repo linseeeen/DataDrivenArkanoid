0fb070c1c10932064675b14f784c96eab060d6a7
Linnéa <c22linkl@student.his.se>
2023-09-05 14:23:29 +0200

    brick is rendering

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Brick", menuName = "Arkanoid/Brick", order = 1)]
 public class Brick : ScriptableObject
 {
-    public string Prefabname;
+    public string Prefabname = "New Brick";
     public Sprite PrefabSprite;
-    public int Health;
-    public int Points;
-    public bool Destructable;
-    
+    public int Health = 1;
+    public int Points = 1;
+    public bool Destructable = true;
 
+    private void Awake()
+    {
+        if (PrefabSprite == null)
+        {
+            Debug.LogWarning("Missing sprite", this);
+        }
+    }
 }
--- Assets/Scripts/BrickManager.cs
+++ Assets/Scripts/BrickManager.cs
@@ -1,19 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Execute always in order for the sprite to show up in the editor. To make changes easier.
+[ExecuteAlways]
 public class BrickManager : MonoBehaviour
 {
     public Brick BrickType;
+    private SpriteRenderer spriteRenderer;
+
+    
     // Start is called before the first frame update
     void Start()
     {
+        name = BrickType.Prefabname;
+        spriteRenderer = GetComponent<SpriteRenderer>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (BrickType == null) {
+            Debug.LogWarning("BrickManager is missing a BrickType. Please add a BrickType");
+        }
+        else
+        {
+            spriteRenderer.sprite = BrickType.PrefabSprite;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            
+        }
     }
 }
